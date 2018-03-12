125554c6d858e1051a82db0d1e547d8be233e61f
Kosuke-Szk <brave.ksk@gmail.com>
2018-03-12 18:32:48 +0900

    company実装

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
+  resources :companies
   devise_for :companies, controllers: {
     sessions:      'companies/sessions',
     passwords:     'companies/passwords',
